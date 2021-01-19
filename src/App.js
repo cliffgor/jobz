d11d9710e3e3430d421f277632e436b0821766b3
cliffgor <clierozigy@gmail.com>
2021-01-19 23:48:25 +0300

    Added pagination to go to the next page. I also added a helper that handles and checks if ther is content on the next page or it's the last page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import React, { useState } from 'react'
 
 import { Container } from 'react-bootstrap'
 import Job from './Job'
+import JobsPagination from './JobsPagination';
 import useFetchJobs from './useFetchJobs'
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
-  const {jobs, loading, error } = useFetchJobs(params, page)
+  const {jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
   return (
     <Container className="my-4">
-      <h1>Welcome to Jobz </h1>
+      <h1 className="mb-4">Welcome to Jobz </h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading....</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   );
 }
