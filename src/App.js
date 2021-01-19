73eb86f66538426daebc87382b404f0e5a5efc2c
cliffgor <clierozigy@gmail.com>
2021-01-19 06:01:20 +0300

    added a job component that houses the Job details, the first test displays the title of jobs in the github job api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 
+import React, { useState } from 'react'
+
 import { Container } from 'react-bootstrap'
+import Job from './Job'
 import useFetchJobs from './useFetchJobs'
 
 function App() {
-  const {jobs, loading, error } = useFetchJobs()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error } = useFetchJobs(params, page)
 
   return (
     <Container>
       {loading && <h1>Loading....</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
