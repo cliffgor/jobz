5c82d19eb4df7c97a46badeb011483ea5f213bff
cliffgor <clierozigy@gmail.com>
2021-01-20 00:27:16 +0300

    Added a form that searches for all Jobs in the Github Api and also indicate if it is remote or fulltime

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Job from './Job'
 import JobsPagination from './JobsPagination';
+import SearchFrom from './SearchFrom';
 import useFetchJobs from './useFetchJobs'
 
 function App() {
@@ -12,9 +13,20 @@ function App() {
   const [page, setPage] = useState(1)
   const {jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
+
+  function handleParamChange(e) { 
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+   }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4">Welcome to Jobz </h1>
+      <SearchFrom params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading....</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
